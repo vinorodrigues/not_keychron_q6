d13d530685eac21a22a52d1701ec44ee752a74d5
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2022-10-29 13:08:24 +1100

    Update config.h

--- config.h
+++ config.h
@@ -28,7 +28,6 @@
     /* EFL/WL Driver Configuration */
     #define WEAR_LEVELING_LOGICAL_SIZE 2048  // Number of bytes "exposed" to the rest of QMK and denotes the size of the usable EEPROM. Page size of stm32l432 is 2k
     #define WEAR_LEVELING_BACKING_SIZE (WEAR_LEVELING_LOGICAL_SIZE * 2)  // Number of bytes used by the wear-leveling algorithm for its underlying storage, and needs to be a multiple of the logical size.
-    #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 2047  // WEAR_LEVELING_LOGICAL_SIZE - 1
 #else
     #error "Please define the compile type in the `rules.mk` file"
 #endif
